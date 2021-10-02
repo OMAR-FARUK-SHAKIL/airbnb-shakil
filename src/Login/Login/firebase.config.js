0f8878003ac626ae1e9fd4b23f81ef62dd1ce3a3
OMAR-FARUK-SHAKIL <omarshakil8@gmail.com>
2021-10-02 18:41:18 +0600

    Login added

---
+++ src/Login/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBdUxEkC2XZM81TiLYmGzuDryc5aLBZ7Js",
+    authDomain: "vaccine-at-home-service.firebaseapp.com",
+    projectId: "vaccine-at-home-service",
+    storageBucket: "vaccine-at-home-service.appspot.com",
+    messagingSenderId: "36582604186",
+    appId: "1:36582604186:web:105b50aa8dccb3cd7b4e23"
+  };
+  export default firebaseConfig;
+  
